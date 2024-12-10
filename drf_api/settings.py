971c6f4602e5543bee0cbedce4e3840f98a5be4b
Michael Sturgeon <1993msturgeon@gmail.com>
2024-12-10 15:02:21 +0000

    Prepare code for deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -204,7 +204,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-DISABLE_COLLECTSTATIC=1
 STATIC_URL = '/static/'
 
 # Default primary key field type
